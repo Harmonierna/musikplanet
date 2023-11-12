118e3b0b35f4aec3f4326421b6d846ada5b451ad
Cedric Karis <cedrickaris@Cedrics-MacBook-Air.local>
2023-11-12 17:38:57 +0100

    feat: add boilerplate code HAR-2

---
+++ kod.py
@@ -0,0 +1,41 @@
+import tkinter as tk
+import math
+import random
+
+class Musikplanet():
+
+    def __init__(self, färgfält, dynamikfält): #två funktioner inom Musikplanets definitionsmängd
+        self.färgfält = färgfält
+        self.dynamikfält = dynamikfält
+
+class Kontrast():
+
+    def __init__(self, y, x, styrka):
+        self.y = y
+        self.x = x
+        self.styrka = 
+
+
+class Färg():
+
+    def __init__(self, y, x, ton):
+        self.y = y
+        self.x = x
+        self.ton = ton
+
+
+class Funktioner():
+    pass
+    #Funktioner som distribuerar färg och kontrast
+    #Och då används för att generera färgfält och dynamikfält
+
+
+#GUI med Musikplanet
+#Över planet kan man dra musen och en viss ton spelas med viss styrka
+
+    
+        
+
+
+
+        
